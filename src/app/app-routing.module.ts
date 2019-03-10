65ddc561abe46df94c60e92400011762f91f80b6
chizzz <mgandrey99@gmail.com>
2019-03-10 17:23:16 +0200

    implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {RouterModule, Routes} from '@angular/router';
+import {WelcomePageComponent} from './welcome-page/welcome-page.component';
+import {FormsPageComponent} from './forms-page/forms-page.component';
+const routes: Routes = [
+  {
+    path: '', component: WelcomePageComponent
+  },
+  {
+    path: 'forms', component: FormsPageComponent
+  }
+];
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    [RouterModule.forRoot(routes) ]
+  ],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {WelcomePageComponent} from './welcome-page/welcome-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {FormsPageComponent} from './forms-page/forms-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    FormsPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
