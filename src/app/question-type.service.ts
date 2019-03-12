c97db1be82c53a27fc299e55e49d5acb13e86167
chizzz <mgandrey99@gmail.com>
2019-03-12 17:38:57 +0200

    add question-type service, implement bar to select type of question but without any functionality

---
+++ src/app/question-type.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionTypeService {
+  private questionTypeBarSubject = new Subject();
+  public questionTypeBar$ = this.questionTypeBarSubject.asObservable();
+  constructor() { }
+  openQuestionTypeBar() {
+    this.questionTypeBarSubject.next(true);
+  }
+  closeQuestionTypeBar() {
+    this.questionTypeBarSubject.next(false);
+  }
+}
--- src/app/questions-form/questions-form.component.ts
+++ src/app/questions-form/questions-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {QuestionService} from '../question.service';
+import {QuestionTypeService} from '../question-type.service';
 
 @Component({
   selector: 'app-questions',
@@ -8,9 +9,13 @@ import {QuestionService} from '../question.service';
 })
 export class QuestionsFormComponent implements OnInit {
   public questions = [];
-  constructor(private questionService: QuestionService) { }
+  public typeBarState;
+  constructor(private questionService: QuestionService, private questionTypeService: QuestionTypeService) { }
 
   ngOnInit() {
+    this.questionTypeService.questionTypeBar$.subscribe(typeBarState => {
+      this.typeBarState = typeBarState;
+    });
   }
   addQuestion() {
     const currentQuestion = this.questionService.addElement(this.questions);
@@ -18,5 +23,11 @@ export class QuestionsFormComponent implements OnInit {
   }
   deleteQuestion(currentQuestion, questionsArray) {
     this.questions = this.questionService.deleteElement(currentQuestion, questionsArray);
+  }
+  openQuestionTypeBar() {
+    this.questionTypeService.openQuestionTypeBar();
+  }
+  closeQuestionTypeBar() {
+    this.questionTypeService.closeQuestionTypeBar();
   }
 }
